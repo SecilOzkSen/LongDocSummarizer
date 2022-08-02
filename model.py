2a18e9fb7b93161337605e2fa07dd695f1372e96
SecilOzkSen <secilozkanoglu@gmail.com>
2022-08-02 16:23:48 +0300

    fixes

--- model.py
+++ model.py
@@ -116,9 +116,11 @@ class LongDocumentSummarizerModel(LightningModule):
             cls_token_index = torch.IntTensor(cls_token_index).to(self.device)
       #      print(cls_token_index)
             cls_token_value = torch.index_select(last_hidden_state[i], 0, cls_token_index.flatten())
-            print(cls_token_value)
+            print(cls_token_value.shape)
             current_dim = cls_token_value.shape[0]
+            print(current_dim)
             padded = F.pad(cls_token_value, pad=(0, pad_dim - current_dim, 0, 0), mode='constant', value=0)
+            print(padded.shape)
             cls_token_values[i, :, :] = padded
         return cls_token_values
 
