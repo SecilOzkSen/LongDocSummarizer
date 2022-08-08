e6c9c78a3a190cfa1cfc7ac448ce172380d431c1
SecilOzkSen <secilozkanoglu@gmail.com>
2022-08-08 18:12:17 +0300

    bug fix

--- trainer.py
+++ trainer.py
@@ -33,8 +33,8 @@ checkpoint_callback = ModelCheckpoint(dirpath="./checkpoints",
                                       filename="summarizer-checkpoint",
                                       save_top_k=1,
                                       verbose=True,
-                                      monitor="val_f1",
-                                      mode="max")
+                                      monitor="val_loss",
+                                      mode="min")
 
 logger = TensorBoardLogger("./logs",
                            name="longformer-summarizer",
